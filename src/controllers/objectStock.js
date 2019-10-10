570e7639bda16cbb1de4999908ce9e3025f93c56
Ramuna <ramunasadauskas@gmail.com>
2019-10-11 00:23:50 +1100

    Update objectStock.js
    added getAllStockRecordIds() and getStockData()

--- src/controllers/objectStock.js
+++ src/controllers/objectStock.js
@@ -4,60 +4,52 @@ const database = databaseConnection();
 
 var util = require('util');
 
-//check if stock already exists
-let checkStockExistsQuery = `SELECT COUNT(*) AS count FROM stock WHERE stock_id = '${this.stockid}'`;
+tests();
 
-//check if product exists
-let checkProductExistsQuery = `SELECT COUNT(*) AS count FROM products WHERE product_id = '${this.productid}'`;
-	
-//add into stock table
-let addStockQuery = `INSERT INTO stock(stock_id, product_id, quantity, exp_date)
-				VALUES('${this.stockid}', '${this.productid}', '${this.quantity}', '${this.expirationdate}')`;
+function tests() {
+	//TEST ADD STOCK
+	/*let stockObj = new Stock(1, 20, `2019/12/15`);
+	stockObj.addStock();*/
 
-//get all stock record ids
-let getAllStockRecordIdsQuery = `SELECT stock_id FROM stock`;
+	//TEST UPDATE STOCK
+	//updateStock(1, 50);
 
-let stockTest = new Stock(444, 1, 20, `2019/12/15`);
-stockTest.addStock();
+	//TEST GET ALL STOCK RECORD IDS
+	/*var allStockRecordIds = getAllStockRecordIds();
+	console.log('stock ids: ' + allStockRecordIds);*/
 
-//updateStock(123, 30);
+	//TEST GET STOCK DATA FOR PASSED IN STOCK IDS
+	allStockRecordIds = [123,124,125,126,127,444,445,446];
+	var stockData = getStockData();
+	console.log('stock data: ' + JSON.stringify(stockData));
+}
 
-var allStockRecordIds = getAllStockRecordIds();
-console.log(allStockRecordIds);
-//console.log(util.inspect(allStockRecordIds));
-
-function Stock(stock_id, product_id, quantity, expiration_date) {
-    this.stockid = stock_id;
+function Stock(product_id, quantity, expiration_date) {
     this.productid = product_id;
     this.quantity = quantity;
-    this.expirationdate = expiration_date;			
-	
+	this.expirationdate = expiration_date;
+
 	this.addStock = function() {
-		database.query(checkStockExistsQuery, (err, results, fields) => {
+		//check if product exists
+		let checkProductExistsQuery = `SELECT COUNT(*) AS count FROM products WHERE product_id = '${this.productid}'`;
+		database.query(checkProductExistsQuery, (err, results, fields) => {
 			if (err) {
 				console.log(err.message);
 			}
 			else if (results[0].count == 0) {
-				database.query(checkProductExistsQuery, (err, results, fields) => {
-					if (err) {
-						console.log(err.message);
-					}
-					else if (results[0].count == 0) {
-						console.log('Product does not exist!');
-					}
-					else {
-						database.query(addStockQuery, (err, results, fields) => {
-							if (err) {
-								console.log(err.message);
-							} else {
-								console.log('Stock added successfully');
-							}
-						});
-					}
-				});
+				console.log('Product does not exist!');
 			}
 			else {
-				console.log('Stock already exists!');
+				//add into stock table
+				let addStockQuery = `INSERT INTO stock(product_id, quantity, exp_date)
+				VALUES('${this.productid}', '${this.quantity}', '${this.expirationdate}')`;
+				database.query(addStockQuery, (err, results, fields) => {
+					if (err) {
+						console.log(err.message);
+					} else {
+						console.log('Stock added successfully');
+					}
+				});
 			}
 		});
 	}
@@ -85,7 +77,6 @@ function Stock(stock_id, product_id, quantity, expiration_date) {
 }
 
 function updateStock(stockId, newQuantity) {
-	console.log('stockId: ' + stockId);
 	let checkStockExistsQuery = `SELECT COUNT(*) AS count FROM stock WHERE stock_id = '${stockId}'`;
 	database.query(checkStockExistsQuery, (err, results, fields) => {
 		if (err) {
@@ -109,19 +100,55 @@ function updateStock(stockId, newQuantity) {
 }
 
 function getAllStockRecordIds() {
-	var stockIds = null;
+	var stockIds = [];
 
-	database.query(getAllStockRecordIdsQuery, (err, results, fields) => {
+	//get all stock record ids
+	let query = `SELECT stock_id FROM stock`;
+	database.query(query, (err, results, fields) => {
 		if (err) {
 			console.log(err.message);
 		}
 		else {
-			stockIds = results;
-			//console.log('results: ' + results);
-			console.log(util.inspect(results));
+			for (var i=0; i<results.length; i++) {
+				stockIds.push(results[i].stock_id);
+			}
+			//console.log('getAllStockRecordIds() stock ids: ' + stockIds);
+
 			console.log('Successfully retreived all stock ids');
+
 		}
 	});
 
 	return stockIds;
+}
+
+
+/**
+ * @return Return array of objects with properties: id, productId, quantity, expirationDate
+ * 
+ */
+function getStockData() {
+	var stockData = [];
+
+	//get all stock record ids
+	let query = `SELECT * FROM stock`;
+	database.query(query, (err, results, fields) => {
+		if (err) {
+			console.log(err.message);
+		}
+		else {
+			for (var i=0; i<results.length; i++) {
+				stockData.push({ id:			 results[i].stock_id,
+								 productId: 	 results[i].product_id,
+								 quantity: 		 results[i].quantity,
+								 expirationDate: results[i].exp_date});
+			}
+			console.log('getStockData() stock data: ' + JSON.stringify(stockData));
+
+			console.log('Successfully retreived stock data');
+
+		}
+	});
+
+	return stockData;
 }
