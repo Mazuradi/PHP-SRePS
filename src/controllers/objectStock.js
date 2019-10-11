2fe3fcb6242f59ee5238d45f6d238c85a8210eb2
Ramuna <ramunasadauskas@gmail.com>
2019-10-11 16:53:55 +1100

    Update objectStock.js
    Add 'already exists' check to add stock function. Remove getAllIds function.

--- src/controllers/objectStock.js
+++ src/controllers/objectStock.js
@@ -2,24 +2,21 @@ const databaseConnection = require('../migrations/DBConnectionDetails');
 //Database connection
 const database = databaseConnection();
 
+var util = require('util');
+
 tests();
 
 function tests() {
 	//TEST ADD STOCK
-	/*let stockObj = new Stock(1, 20, `2019/12/15`);
-	stockObj.addStock();*/
+	let stockObj = new Stock(1, 20, `2019/12/16`);
+	stockObj.addStock();
 
 	//TEST UPDATE STOCK
 	//updateStock(1, 50);
 
-	//TEST GET ALL STOCK RECORD IDS
-	var allStockRecordIds = getAllStockRecordIds();
-	console.log('stock ids: ' + allStockRecordIds);
-
-	//TEST GET STOCK DATA FOR PASSED IN STOCK IDS
-	allStockRecordIds = [123,124,125,126,127,444,445,446];
-	var stockData = getStockData();
-	console.log('stock data: ' + JSON.stringify(stockData));
+	//TEST GET STOCK DATA
+	/*var stockData = getStockData();
+	console.log('stock data: ' + JSON.stringify(stockData));*/
 }
 
 function Stock(product_id, quantity, expiration_date) {
@@ -38,14 +35,27 @@ function Stock(product_id, quantity, expiration_date) {
 				console.log('Product does not exist!');
 			}
 			else {
-				//add into stock table
-				let addStockQuery = `INSERT INTO stock(product_id, quantity, exp_date)
-				VALUES('${this.productid}', '${this.quantity}', '${this.expirationdate}')`;
-				database.query(addStockQuery, (err, results, fields) => {
+				let checkStockExistsQuery = `SELECT COUNT(*) AS count FROM stock 
+										 	 WHERE product_id = '${this.productid}'
+											 AND exp_date = '${this.expirationdate}'`;
+				database.query(checkStockExistsQuery, (err, results, fields) => {
 					if (err) {
 						console.log(err.message);
-					} else {
-						console.log('Stock added successfully');
+					}
+					else if (results[0].count == 0) {
+						//add into stock table
+						let addStockQuery = `INSERT INTO stock(product_id, quantity, exp_date)
+						VALUES('${this.productid}', '${this.quantity}', '${this.expirationdate}')`;
+						database.query(addStockQuery, (err, results, fields) => {
+							if (err) {
+								console.log(err.message);
+							} else {
+								console.log('Stock added successfully');
+							}
+						});
+					}
+					else {						
+						console.log('Stock with the same product and expiration date already exsist!');
 					}
 				});
 			}
@@ -76,31 +86,6 @@ function updateStock(stockId, newQuantity) {
 	});
 }
 
-//not sure if we actually need this function??
-function getAllStockRecordIds() {
-	var stockIds = [];
-
-	//get all stock record ids
-	let query = `SELECT stock_id FROM stock`;
-	database.query(query, (err, results, fields) => {
-		if (err) {
-			console.log(err.message);
-		}
-		else {
-			for (var i=0; i<results.length; i++) {
-				stockIds.push(results[i].stock_id);
-			}
-			console.log('getAllStockRecordIds() stock ids: ' + stockIds);
-
-			console.log('Successfully retreived all stock ids');
-
-		}
-	});
-
-	return stockIds;
-}
-
-
 /**
  * @return Array of objects with properties: id, productId, quantity, expirationDate
  * 
@@ -108,7 +93,7 @@ function getAllStockRecordIds() {
 function getStockData() {
 	var stockData = [];
 
-	//get all stock record ids
+	//get all stock record data
 	let query = `SELECT * FROM stock`;
 	database.query(query, (err, results, fields) => {
 		if (err) {
@@ -127,6 +112,7 @@ function getStockData() {
 
 		}
 	});
+	console.log('getStockData() stock data 2: ' + JSON.stringify(stockData));
 
 	return stockData;
 }
