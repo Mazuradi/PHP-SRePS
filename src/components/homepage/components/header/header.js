875f837bed6a51daf16258baeb62fa0ff2da6e7f
Isaac Renner <isaac@trunkplatform.com>
2019-10-31 13:24:19 +1100

    Added Signout Button

--- src/components/homepage/components/header/header.js
+++ src/components/homepage/components/header/header.js
@@ -10,6 +10,7 @@ const Header = () => (
     <HeaderButton buttonText={"Stock"} linkURL={"/Stock"} />
     <HeaderButton buttonText={"Transactions"} linkURL={"/Transactions"} />
     <HeaderButton buttonText={"Reports"} linkURL={"/Reports"} />
+    <HeaderButton buttonText={"Sign Out"} onClick={localStorage.setItem('user', null)} />
   </Wrapper>
 );
 
